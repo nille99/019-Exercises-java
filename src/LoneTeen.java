6fd0197fdcada1d69c3879a5b9e2ef3b7d5153f6
Niklas Sawensten <nisaw99@hotmail.com>
2017-11-13 14:30:39 +0100

    Add main class

--- src/LoneTeen.java
+++ src/LoneTeen.java
@@ -1,12 +1,6 @@
 
 public class LoneTeen {
 
-	public static void main(String[] args) {
-
-		LoneTeen loneTeen = new LoneTeen();
-		loneTeen.loneTeen(21,1);
-	}
-
 
 	public boolean loneTeen(int a, int b) {
 		  boolean aTeen = (a >= 13 && a <= 19);
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		LoneTeen loneTeen = new LoneTeen();
+		loneTeen.loneTeen(21,1);
+	}
+
+}
